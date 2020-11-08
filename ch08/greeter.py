94547ed38483368eab4ba73088b2a3f1ae96e3e4
Edson Jesus <edsonsjesus@gmail.com>
2020-11-08 16:06:17 -0300

    Tell what I'm learning in this chapter and one of my favorite books.

--- ch08/greeter.py
+++ ch08/greeter.py
@@ -3,4 +3,16 @@ def greet_user(user_name):
     print(f'Hello, {user_name.title()}!')
 
 
+def display_message():
+    """Tell what I'm learning in this chapter"""
+    print("I'm learning how to make great functions in this chapter!")
+
+
+def favorite_book(title):
+    """Tell one of my favorite books"""
+    print(f"One of my favorite books is {title.title()}.")
+
+
 greet_user('edson')
+display_message()
+favorite_book('the lord of the rings')
